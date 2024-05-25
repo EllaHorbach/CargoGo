24959ea774bb9c3720bb45beab83a66a6bf06661
EllaHorbach <ellamarit.horbach@leerling.piustien.net>
2024-05-25 17:32:48 +0200

    update_kleuren_bakfiets

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-const wrapper = document.quer
+
+
+// JavaScript om de afbeelding te wijzigen op basis van de geselecteerde kleur
+document.addEventListener('DOMContentLoaded', function () {
+    const colorSelect = document.querySelectorAll('.colour-select div');
+
+    // Itereer over alle kleurselectie-elementen en voeg een klikgebeurtenis toe
+    colorSelect.forEach(function (element) {
+        element.addEventListener('click', function () {
+            // Haal de kleur op van het ID-attribuut van het element
+            const color = element.id;
+            // Roep de changeImage-functie aan en geef de geselecteerde kleur door
+            changeImage(color);
+        });
+    });
+});
+
+function changeImage(color) {
+    const productImg = document.querySelector('.productImg'); // Gebruik querySelector omdat er slechts één element met deze klasse is
+    switch (color) {
+        case 'blue':
+            productImg.src = './img_producten/bak_fiets_blauw.jpg';
+            break;
+        case 'red':
+            productImg.src = './img_producten/bak_fiets_rood.jpg';
+            break;
+        case 'white':
+            productImg.src = './img_producten/bak_fiets_wit.jpg';
+            break;
+        default:
+            break;
+    }
+}
+
+
